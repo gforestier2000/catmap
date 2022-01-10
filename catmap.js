8c0858a5af4c9758950a4f370341e63e158c71a3
Guillaume Forestier <gforestier2000@yahoo.fr>
2022-01-10 17:46:25 +0100

    js3

--- catmap.js
+++ catmap.js
@@ -2,6 +2,6 @@ console.log("Hello Cat Map ! Where is the cat ? ");
 
 let buttonUP = document.getElementById("up");
 
-buttonUP.addEventListener(event, () => {
+buttonUP.addEventListener(ev,(ev)=>{
     console.log("UP");
 });
